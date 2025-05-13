3168e7159619f6924ea7086085f7a5ab1885b153
Jeong-Yunmi <yunmiii0226@khu.ac.kr>
2025-05-13 18:42:30 +0900

    Initial project setup with Spring Boot

---
+++ src/main/java/com/securitydemo/vulnerable_webapp/VulnerableWebappApplication.java
@@ -0,0 +1,13 @@
+package com.securitydemo.vulnerable_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnerableWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnerableWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securitydemo/vulnerable_webapp/VulnerableWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitydemo.vulnerable_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnerableWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
